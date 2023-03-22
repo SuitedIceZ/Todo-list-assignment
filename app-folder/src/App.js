ff7647c65d6f87da6ea7c7b4c694ca138a811b5b
SuitedIceZ <okshopm575@gmail.com>
2023-03-22 17:16:25 +0700

    [APP] style: enable prettier formater

--- app-folder/src/App.js
+++ app-folder/src/App.js
@@ -1,46 +1,40 @@
-import React, {useState} from 'react';
-import TodoList from './components/TodoList';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
 function App() {
-
   const [todoList, setTodoList] = useState(["task 1"]);
   const [inputText, setText] = useState("a task");
 
-  const onChangeInputText = ({target: {value}}) => {
+  const onChangeInputText = ({ target: { value } }) => {
     console.log("onChangeInputText: " + value);
-    setText(value)
-  }
-  
-  const addTask = () => {
+    setText(value);
+  };
 
-    setTodoList([
-      ...todoList,
-      inputText
-    ])
+  const addTask = () => {
+    setTodoList([...todoList, inputText]);
 
     //clear text
     setText("");
-
-  }
+  };
 
   const removeTask = (index) => {
     console.log("removeTask: " + index);
-    
+
     let newTodoList = [...todoList];
 
     newTodoList.splice(index, 1);
     setTodoList(newTodoList);
-  }
+  };
 
   return (
     <div>
       <h1>My First React App</h1>
       <div>
-        <input type="text" value={inputText} onChange={ onChangeInputText }/>
+        <input type="text" value={inputText} onChange={onChangeInputText} />
         <button onClick={addTask}>Add</button>
       </div>
 
       <div>
-        <TodoList todoList={todoList} removeTask={removeTask}/>
+        <TodoList todoList={todoList} removeTask={removeTask} />
       </div>
     </div>
   );
